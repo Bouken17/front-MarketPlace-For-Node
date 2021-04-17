8425447eeb052b5b3f1cc04fe72ec2f2cbb2b2e7
Bouken17 <gbouken95@gmail.com>
2021-04-17 15:22:50 +0000

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,38 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { ShopComponent } from './components/shop/shop.component';
+import { SigninComponent } from './components/auth/signin/signin.component';
+import { SignupComponent } from './components/auth/signup/signup.component';
+import { SingleProductComponent } from './components/shop/single-product/single-product.component';
+import { AddProductComponent } from './components/shop/add-product/add-product.component';
+import { EditProductComponent } from './components/shop/edit-product/edit-product.component';
+import { CartComponent } from './components/shop/cart/cart.component';
+import { HeaderComponent } from './components/partials/header/header.component';
+import { FooterComponent } from './components/partials/footer/footer.component';
+import { NotFoundComponent } from './components/partials/not-found/not-found.component';
+import { HeaderPageComponent } from './components/partials/header-page/header-page.component';
+import { QuickViewModalComponent } from './components/partials/modal/quick-view-modal/quick-view-modal.component';
+import { AddToCartModalComponent } from './components/partials/modal/add-to-cart-modal/add-to-cart-modal.component';
+import { DeleteProductModalComponent } from './components/partials/modal/delete-product-modal/delete-product-modal.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ShopComponent,
+    SigninComponent,
+    SignupComponent,
+    SingleProductComponent,
+    AddProductComponent,
+    EditProductComponent,
+    CartComponent,
+    HeaderComponent,
+    FooterComponent,
+    NotFoundComponent,
+    HeaderPageComponent,
+    QuickViewModalComponent,
+    AddToCartModalComponent,
+    DeleteProductModalComponent
   ],
   imports: [
     BrowserModule
